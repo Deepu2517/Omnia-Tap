aeedd09fe724377b6f102bc2f03b6f5955698604
Deepu2517 <jarupla.deepak@gmail.com>
2017-09-09 21:44:14 +0530

    increased performance

--- app/src/main/java/in/desireplace/waytogo/extras/FirebaseIDService.java
+++ app/src/main/java/in/desireplace/waytogo/extras/FirebaseIDService.java
@@ -11,7 +11,6 @@ public class FirebaseIDService extends FirebaseInstanceIdService {
 
     @Override
     public void onTokenRefresh() {
-        // Get updated InstanceID token.
         String refreshedToken = FirebaseInstanceId.getInstance().getToken();
         Log.d(Constants.TAG, "Refreshed token: " + refreshedToken);
 
@@ -19,15 +18,7 @@ public class FirebaseIDService extends FirebaseInstanceIdService {
         sendRegistrationToServer(refreshedToken);
     }
 
-    /**
-     * Persist token to third-party servers.
-     *
-     * Modify this method to associate the user's FCM InstanceID token with any server-side account
-     * maintained by your application.
-     *
-     * @param token The new token.
-     */
     private void sendRegistrationToServer(String token) {
-        // Add custom implementation, as needed.
+
     }
 }
--- app/src/main/java/in/desireplace/waytogo/extras/MyFirebaseMessagingService.java
+++ app/src/main/java/in/desireplace/waytogo/extras/MyFirebaseMessagingService.java
@@ -12,9 +12,6 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService {
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         // TODO: Handle FCM messages here.
-        // If the application is in the foreground handle both data and notification messages here.
-        // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated.
         Log.d(Constants.TAG, "From: " + remoteMessage.getFrom());
         Log.d(Constants.TAG, "Notification Message Body: " + remoteMessage.getNotification().getBody());
     }
